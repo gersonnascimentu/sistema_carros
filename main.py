d645d830e4544b0fd60cbab52e221749fdb5cc7e
Gerson Nascimentu <gerson.lima.nb@gmail.com>
2025-11-27 12:49:29 -0300

    att

--- main.py
+++ main.py
@@ -2,12 +2,45 @@
 
 from models.carro import Carro
 
+lista = Carro("", "", "").carregar_todos
+
+def marcas_principais():
+    marca = "Marca: Fiat"
+    modelo = "Modelo: Uno"
+    ano = "Ano: 2026"
+    carro = Carro(marca, modelo, ano)
+    carro.salvar_carro()
+
+    marca = ("Marca: Chevrolet")
+    modelo = ("Modelo: Onix")
+    ano = ("Ano: 2026")
+    carro = Carro(marca, modelo, ano)
+    carro.salvar_carro()
+
+    marca = ("Marca: Volkswagen")
+    modelo = ("Modelo: Fusca")
+    ano = ("Ano: 2026")
+    
+    carro = Carro(marca, modelo, ano)
+    carro.salvar_carro()
+
+
+
 def menu():
     print("\n=== SISTEMA DE CADASTRO DE CARROS ===")
     print("1 - Cadastrar carro")
     print("2 - Listar carros")
     print("0 - Sair")
     return input("Escolha uma opção: ")
+
+def menu2():
+    print("\n=== ESCOLHA A MARCA ===")
+    print("1 - Fiat")
+    print("2 - Chevrolet")
+    print("3 - Volkswagen")
+    print("4 - Outras Opções de cadastro")
+    print("0 - Voltar")
+    
 
 while True:
     opcao = menu()
